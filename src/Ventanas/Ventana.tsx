2484446b786c29e680a90d7b49a08394eced8414
jorge <jorge.lacayo@utp.ac.pa>
2022-12-01 15:07:52 -0500

    Probando

---
+++ src/Ventanas/Ventana.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/stack';
+//import Login from './Login';
+//import Perfil from './Perfil';
+
+//const Stack = createStackNavigator();
+
+/*const NoticiasStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+
+      <Stack.Screen name="Perfil" component={Perfil} />
+    </Stack.Navigator>
+  );
+};
+
+export default NoticiasStack;*/
